3a9c871a2d9433e1fc669a1f8673bd29f740e934
DanielProgrammer64 <103775773+DanielProgrammer64@users.noreply.github.com>
2022-11-11 23:59:03 -0300

    Componentes
    .

---
+++ config.js
@@ -0,0 +1,17 @@
+ import * as firebase from "firebase";
+
+//inicialize seu banco de dados
+const firebaseConfig = {
+  apiKey: "AIzaSyBd38Br6tZBz8OiKedpP3BxpkEj_EM3OJE",
+  authDomain: "projeto-60-85d0b.firebaseapp.com",
+  databaseURL: "https://projeto-60-85d0b-default-rtdb.firebaseio.com",
+  projectId: "projeto-60-85d0b",
+  storageBucket: "projeto-60-85d0b.appspot.com",
+  messagingSenderId: "200962596747",
+  appId: "1:200962596747:web:4725ac53b3e4d586a1998e",
+  measurementId: "G-2JE91RB9ZQ",
+};
+  export default firebase.database()
+ 
+
+  
